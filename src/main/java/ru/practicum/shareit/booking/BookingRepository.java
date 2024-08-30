e688ccc59183af797b1d2ff94c1df6eabf2db3d5
AntonBelugin <svantonio910@yandex.ru>
2024-08-30 18:13:46 +0300

    sprint 15 (add-bookings) v2 (исправлены замечания после 1го ревью)

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     default Booking getBookingById(long id) {
         return findById(id).orElseThrow(() -> new NotFoundException("Бронирование с id " + id + " не существует"));
-
     }
     List<Booking> findByBookerIdOrderByStartDesc(Long id);
 
