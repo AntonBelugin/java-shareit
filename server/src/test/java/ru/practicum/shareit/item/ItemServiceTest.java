d1f7d1351b759d8fcdd38dad0b53979eaa2f2fd9
anton <svantonio910@yandex.ru>
2024-10-08 21:16:33 +0300

    sprint 16 v1

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
@@ -3,18 +3,11 @@ package ru.practicum.shareit.item;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.item.dto.ItemDtoWithComments;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
-
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
 @Transactional
 @SpringBootTest(
--- server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -3,15 +3,10 @@ package ru.practicum.shareit.user;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
 @Transactional
 @SpringBootTest(
